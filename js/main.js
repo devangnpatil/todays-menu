2885dc418f2245ec8025ec31d07ffbc8b6021873
devang <devang.patil@upgrad.com>
2019-01-24 10:54:08 +0530

    add

---
+++ js/main.js
@@ -0,0 +1,49 @@
+var MenuModel = Backbone.Model.extend({
+	url: 'db/menu.json'
+});
+var menu = new MenuModel();
+var AppView = Backbone.View.extend ({
+// el - stands for element. Every view has an element associated with HTML content, will be rendered. 
+el: '.todays-menu-wrapper',
+
+// It's the first function called when this view is instantiated.
+initialize: function() {
+	 this.render(); 
+},
+
+today: '',
+
+events: {
+	'click .btn-show-menu': 'fetchMenu' 
+},
+
+filterMenu: function(data){
+	var data = data.toJSON();
+	var meal = $('.select-meal').val(); //lunch/dinner
+	var menu = $('.select-menu').val(); //veg/nonveg
+	var prefer = _.where(data, {'type':menu});
+	todaysMenu = prefer[0].menu[Math.floor(Math.random()*10)];
+	$('.show-todays-menu').text('').text(todaysMenu);
+},
+
+fetchMenu: function(e){
+	var that = this;
+	e.preventDefault();
+		menu.fetch({ 
+		url: "db/menu.json", 
+		success: function(data) {
+			that.filterMenu(data);
+			},
+		error: function(){
+			 console.log('There was some error in loading and processing the JSON file');
+		}
+	});
+},
+
+render: function() {
+	var today = moment().format('dddd');
+	$('.today').text(today);
+	this.today = today
+}
+});
+var appView = new AppView();
